6e6e02868f93d8b44f4bba157753973e8ffda92d
Andrew Spiers <andrew@andrewspiers.net>
2016-02-21 18:34:04 +1100

    Use dictionary in command command module

--- control.py
+++ control.py
@@ -1,53 +1,65 @@
+cmd = {}
 
 
-#Prefix to all control codes
-COMMAND = '\xfe'
+# Prefix to all control codes
+#############################
+cmd['COMMAND'] = '\xfe'
 
-#Basic commands
-BACKLIGHT_ON = b'\x42'
-BACKLIGHT_OFF = b'\x46'
-SET_BRIGHTNESS = b'\x99'
-SET_CONTRAST = b'\x50'
-AUTOSCROLL_ON = b'\x51'
-AUTOSCROLL_OFF = b'\x52'
-CLEAR_SCREEN = b'\x58'
-CHANGE_STARTUP = b'\x40'
+# Basic commands
+cmd['BACKLIGHT_ON'] = b'\x42'
+cmd['BACKLIGHT_OFF'] = b'\x46'
+cmd['SET_BRIGHTNESS'] = b'\x99'
+cmd['SET_CONTRAST'] = b'\x50'
+cmd['AUTOSCROLL_ON'] = b'\x51'
+cmd['AUTOSCROLL_OFF'] = b'\x52'
+cmd['CLEAR_SCREEN'] = b'\x58'
+cmd['CHANGE_STARTUP'] = b'\x40'
 
-#Moving and changing the cursor
-CURSOR_POSITION = b'\x47'
-CURSOR_HOME = b'\x48'
-CURSOR_BACK = b'\x4c'
-CURSOR_FORWARD = b'\x4d'
-CURSOR_UNDERLINE_ON = b'\x4a'
-CURSOR_UNDERLINE_OFF = b'\x4b'
-CURSOR_BLOCK_ON = b'\x53'
-CURSOR_BLOCK_OFF = b'\x54'
+# Moving and changing the cursor
+################################
+cmd['CURSOR_POSITION'] = b'\x47'
+cmd['CURSOR_HOME'] = b'\x48'
+cmd['CURSOR_BACK'] = b'\x4c'
+cmd['CURSOR_FORWARD'] = b'\x4d'
+cmd['CURSOR_UNDERLINE_ON'] = b'\x4a'
+cmd['CURSOR_UNDERLINE_OFF'] = b'\x4b'
+cmd['CURSOR_BLOCK_ON'] = b'\x53'
+cmd['CURSOR_BLOCK_OFF'] = b'\x54'
 
-#RGB Backlight and LCD Size
-BACKLIGHT_COLOR = b'\xd0' # needs three bytes following, R,G,B.
-LCD_SIZE = b'\xd1'
-CUSTOM_CHARACTER_CREATE = '\x4e' # needs one byte 0-7 following to indicate
-                                 # slot, followed by 8 bytes for appearance.
-CUSTOM_CHARACTER_SAVE = '\xc1'   # Save the custom character.
-CUSTOM_CHARACTER_LOAD = '\xc0'   # Load specified bank of custom characters
+# RGB Backlight and LCD Size
+#############################
+cmd['BACKLIGHT_COLOR'] = b'\xd0'  # needs three bytes following, R,G,B.
+cmd['LCD_SIZE'] = b'\xd1'
+# needs one byte 0-7 following to indicate slot, then 8 bytes for appearance.
+cmd['CUSTOM_CHARACTER_CREATE'] = '\x4e'
+# Save the custom character.
+cmd['CUSTOM_CHARACTER_SAVE'] = '\xc1'
+# Load specified bank of custom characters
+cmd['CUSTOM_CHARACTER_LOAD'] = '\xc0'
 
-#General Purpose Output
+# General Purpose Output
 
-#GPO MAP
+# GPO MAP
 
-#GPO1 : PB0
-#GPO2 : PC2
-#GPO3 : PC4
-#GPO4 : PC7
+# GPO1 : PB0
+# GPO2 : PC2
+# GPO3 : PC4
+# GPO4 : PC7
 
-GPO_OFF = '\x56'
-GPO_ON  = '\x56'
+cmd['GPO_OFF'] = '\x56'
+cmd['GPO_ON'] = '\x56'
 
 
 # compound commands
-clear = COMMAND + CLEAR_SCREEN + COMMAND + CURSOR_HOME
+###################
+cmd['clear'] = ''.join(
+    (cmd['COMMAND'], cmd['CLEAR_SCREEN'], cmd['COMMAND'], cmd['CURSOR_HOME']))
 
-red = COMMAND + BACKLIGHT_COLOR + b'\xff\x00\x00'
-green = COMMAND + BACKLIGHT_COLOR + b'\x00\xff\x00'
-blue = COMMAND + BACKLIGHT_COLOR + b'\x00\x00\xff'
-white = COMMAND + BACKLIGHT_COLOR + b'\xff\xff\xff'
+cmd['red'] = ''.join(
+    (cmd['COMMAND'], cmd['BACKLIGHT_COLOR'], b'\xff\x00\x00'))
+cmd['green'] = ''.join(
+    (cmd['COMMAND'], cmd['BACKLIGHT_COLOR'], b'\x00\xff\x00'))
+cmd['blue'] = ''.join(
+    (cmd['COMMAND'], cmd['BACKLIGHT_COLOR'], b'\x00\x00\xff'))
+cmd['white'] = ''.join(
+    (cmd['COMMAND'], cmd['BACKLIGHT_COLOR'], b'\xff\xff\xff'))
